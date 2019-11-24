c4687ef74d4e0b12ce0e5df67a830edeafb23587
Hangyu Ye <jerry.hangyu@gmail.com>
2019-11-24 15:08:12 +0800

    Update DoubleBall.swift

--- DoubleBall.swift
+++ DoubleBall.swift
@@ -1,16 +1,24 @@
 import Foundation
 
-var numbers: [UInt32] = []
-var pool: [UInt32] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
-let targetNum = arc4random_uniform(33)
+var totalNumberInPool: UInt32 = 33
+var redPool: [Int] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
+var bulePool: [Int] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 
 var counter = 0
 
-
-for _ in 1...7 {
-    let index = arc4random_uniform(33)
-    let redBall: UInt32 = pool[Int(index)]
-    numbers.append(redBall)
+func winningNumber() -> [Int] {
+    var res: [Int] = []
+    var pool = redPool
+    var totalNumInPool = totalNumberInPool
+    for i in 0...6 {
+        let arrayIndex = Int(arc4random_uniform(totalNumInPool - UInt32(i)))
+        res.append(pool[arrayIndex])
+        pool.remove(at: arrayIndex)
+    }
+    print (res.sorted())
+    return res.sorted()
 }
 
-print(numbers.sorted())
+for _ in 1...100 {
+    winningNumber()
+}
